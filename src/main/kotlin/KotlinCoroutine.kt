410ed0a1135e544ca1c95ad31bbc511dd65a912c
Sagar Avhad <avhad.avhad@gmail.com>
2020-09-20 10:28:05 +0530

    used launch instead of GlobalScope.launch

--- src/main/kotlin/KotlinCoroutine.kt
+++ src/main/kotlin/KotlinCoroutine.kt
@@ -4,15 +4,15 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
-fun main() = runBlocking {
+fun main() = runBlocking {// main thread
     println("Main work starts: ${Thread.currentThread().name}")
-    GlobalScope.launch {
-        println("Fake work starts: ${Thread.currentThread().name}")
-        delay(1000) //pretend some work like uploading file
-        println("Fake work ends: ${Thread.currentThread().name}")
+    launch {// main thread
+        println("Fake work starts: ${Thread.currentThread().name}") // main thread
+        delay(1000) //suspend coroutine and make our thread free
+        println("Fake work ends: ${Thread.currentThread().name}") // main thread or other thread
     }
 
-    delay(2000)
+    delay(2000) // this is not right way
 
     println("Main work ends: ${Thread.currentThread().name}")
 }
