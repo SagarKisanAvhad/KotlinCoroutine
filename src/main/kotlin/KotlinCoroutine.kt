060b1d81e05f2101cd0327bbb1b6704133cb34ac
Sagar Avhad <avhad.avhad@gmail.com>
2020-09-20 10:57:43 +0530

    learn using async and await

--- src/main/kotlin/KotlinCoroutine.kt
+++ src/main/kotlin/KotlinCoroutine.kt
@@ -3,13 +3,15 @@ import kotlin.concurrent.thread
 
 fun main() = runBlocking {// T1 = main thread
     println("Main work starts: ${Thread.currentThread().name}")
-    val job: Job = launch {// T1 = main thread
+    val jobDeferred: Deferred<Int> = async {// T1 = main thread
         println("Fake work starts: ${Thread.currentThread().name}") // main thread(T1)
         delay(1000) //suspend coroutine and make our thread free
         println("Fake work ends: ${Thread.currentThread().name}") // main thread(T1) or other thread
+        33
     }
 
-    job.join() // this wait till coroutine finish its work.
-
+    // jobDeferred.join() // returned value Not needed + need to wait till coroutine
+    val returnValue = jobDeferred.await() // returned value needed + need to wait till coroutine
+    println(returnValue)
     println("Main work ends: ${Thread.currentThread().name}") // this line will execute after coroutine finish its work
 }
