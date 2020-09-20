0acce38337988db7206a5725167d2978c81c5f2a
Sagar Avhad <avhad.avhad@gmail.com>
2020-09-20 10:31:53 +0530

    used join() on job we get from launch

--- src/main/kotlin/KotlinCoroutine.kt
+++ src/main/kotlin/KotlinCoroutine.kt
@@ -1,18 +1,15 @@
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import kotlin.concurrent.thread
 
-fun main() = runBlocking {// main thread
+fun main() = runBlocking {// T1 = main thread
     println("Main work starts: ${Thread.currentThread().name}")
-    launch {// main thread
-        println("Fake work starts: ${Thread.currentThread().name}") // main thread
+    val job: Job = launch {// T1 = main thread
+        println("Fake work starts: ${Thread.currentThread().name}") // main thread(T1)
         delay(1000) //suspend coroutine and make our thread free
-        println("Fake work ends: ${Thread.currentThread().name}") // main thread or other thread
+        println("Fake work ends: ${Thread.currentThread().name}") // main thread(T1) or other thread
     }
 
-    delay(2000) // this is not right way
+    job.join() // this wait till coroutine finish its work.
 
-    println("Main work ends: ${Thread.currentThread().name}")
+    println("Main work ends: ${Thread.currentThread().name}") // this line will execute after coroutine finish its work
 }
